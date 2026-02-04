acd9015839c6c4d7a1f5c301590fb1fccbd22fdd
anshuman <anshuman.sugandhi7816@gmail.com>
2026-02-04 18:01:23 +0530

    Backend ready for deployment

--- api/main.py
+++ api/main.py
@@ -45,8 +45,12 @@ def semantic(q: str):
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://lexarena-eight.vercel.app/"
+    ],
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
